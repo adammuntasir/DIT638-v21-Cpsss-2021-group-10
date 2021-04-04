5d18936d3e68b732b07d40e32862b97912d370af
Ali <alika@student.chalmers.se>
2021-04-04 12:55:36 +0200

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -3,7 +3,7 @@
 /*including prime checker as header file*/
 
 int main(int argc, char** argv) {
-if (argc == 2) {
+if (argc == 2) { /*checking is correct number of arguments given*/
 int number = std::stoi(argv[1]);
     PrimeChecker pc;
     std::cout << "Group 10; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
