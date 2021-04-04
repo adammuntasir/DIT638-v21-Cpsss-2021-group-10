4d291105e8f95ba235b5de3d4859129b36dbd09b
Ali <alika@student.chalmers.se>
2021-04-04 10:34:48 +0200

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -7,5 +7,5 @@ int number = std::stoi(argv[1]);
     PrimeChecker pc;
     std::cout << "Group 10; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
 }
-    return 0; /*a comment*/
+    return 0; /*a good comment*/
 }
